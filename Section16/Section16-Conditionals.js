341791e4bc06ad8662a2560a9df882a7029c253e
Trevor <trevllew751@vt.edu>
2020-12-26 17:25:26 -0500

    Section 16

---
+++ Section16/Section16-Conditionals.js
@@ -0,0 +1,40 @@
+// console.log("Before Conditional");
+// let rand = Math.random();
+// if (rand < 0.5) {
+//     console.log("The number is less than 0.5")
+//     console.log(rand)
+// }
+
+// const dayOfWeek = "Saturday";
+//
+// if (dayOfWeek === "Monday") {
+//     console.log("I hate mondays")
+// } else if (dayOfWeek === "Saturday") {
+//     console.log("I love Saturdays!")
+// } else if (dayOfWeek === "Friday") {
+//     console.log("Fridays are good")
+// }
+
+// const age = prompt("How old are you?");
+// if (age < 5) {
+//     console.log("You are a baby. You get in for free!");
+// } else if (age < 10) {
+//     console.log("You are a child. Price is $10.")
+// } else if (age < 65) {
+//     console.log("You are an adult. Price is $20.")
+// } else {
+//     console.log("You are a senior. Price is $10.")
+// }
+
+// const password = prompt("Please enter a paswword");
+//
+// if (password.length >= 6) {
+//     console.log("Password is strong")
+//     if (password.indexOf(" ") === -1) {
+//         console.log("Valid password")
+//     } else {
+//         console.log("Password cannot have space")
+//     }
+// } else {
+//     console.log("Password too short")
+// }
---
+++ Section16/Section16-RunningJS.js
@@ -0,0 +1,4 @@
+console.log("Hello World");
+console.log("Yuh")
+let variable = 1+10;
+console.log(variable);
